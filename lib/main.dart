63c6914b6b25140e37ea28656024fc135fc0e11d
Janethra Dissanayake <129816926+chaki-web@users.noreply.github.com>
2025-04-29 23:07:03 +0530

    Changed the 54th line, Please concider. Thank you.

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Do.lk'),
+            const Text('Haritha Nagare mal kumari awillaa!!'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
