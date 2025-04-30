5652605b2c95166bb189c9add4355731b331a07c
Janethra Dissanayake <129816926+chaki-web@users.noreply.github.com>
2025-04-30 09:43:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('Haritha Nagare mal kumari awillaa!'),
+            const Text('Haritha Nagare mal kumari awillaa! Aneee'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
